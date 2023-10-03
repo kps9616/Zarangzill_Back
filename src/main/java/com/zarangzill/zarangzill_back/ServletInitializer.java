19c5266e1ddc4f8fdd84b98b369d574dc810e0ab
bragging <kps9616@gmail.com>
2023-10-03 17:51:19 +0900

    초기 커밋

---
+++ src/main/java/com/zarangzill/zarangzill_back/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zarangzill.zarangzill_back;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZarangzillBackApplication.class);
+    }
+
+}
---
+++ src/test/java/com/zarangzill/zarangzill_back/ZarangzillBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarangzill.zarangzill_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarangzillBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
