85259ec2e9645cdb15b091d8b25f1c66443fb35a
elax89 <elax89 @H_Work>
2023-11-07 23:30:08 +0900

    초기 스프링 부트 커밋

---
+++ Zarangzill_Back/src/test/java/com/main/demo/ZarangzillBackApplicationTests.java
@@ -0,0 +1,14 @@
+package com.main.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest
+class ZarangzillBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
