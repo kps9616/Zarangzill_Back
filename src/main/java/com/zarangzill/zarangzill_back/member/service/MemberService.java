c9021756b86677970d086b45cb6cebafb2696451
elax89@gmail.com <elax89@gmail.com>
2024-01-14 23:48:05 +0900

    Member 기본 구조 변경

--- src/main/java/com/zarangzill/zarangzill_back/member/service/MemberService.java
+++ src/main/java/com/zarangzill/zarangzill_back/member/service/MemberService.java
@@ -1,8 +1,5 @@
 package com.zarangzill.zarangzill_back.member.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface MemberService {
     int createMember(MemberDTO memberDto);
 
